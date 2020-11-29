c3837055ce3b50143651db867bc4099bd214b84c
Nolan <nolan.lu@veztan.com>
2020-11-29 21:01:45 +0800

    add integer checking

--- move.py
+++ move.py
@@ -1,22 +1,31 @@
 import numpy as np;
+import sys
 
-
-def moves() -> object:
+def moves():
     # Define direction
-    U = np.array([0, 1])
-    D = np.array([0, -1])
-    L = np.array([-1, 0])
-    R = np.array([1, 0])
+    direction_u = np.array([0, 1])
+    direction_d = np.array([0, -1])
+    direction_l = np.array([-1, 0])
+    direction_r = np.array([1, 0])
+    max_retry = 6
+    # Define original position
+    for i in range(0,max_retry):
+        try:
+            original_x, original_y = input("Enter numeric original position:").replace(',', ' ').split()
+            original_x = int(original_x)
+            original_y = int(original_y)
+            original_position = [original_x, original_y]
+            print("Original position:", original_position)
+            break
+        except ValueError:
+            print("Error times:", i, "Please input two integer.","Retry times left:", max_retry-i-1)
+        if i == 5:
+            print("Error times:", i+1, "This program will be stopped here")
+            sys.exit(1)
 
     # Define moving instruction
+    print("Please enter the instruction. e.g. UDLR")
     moves_lst = input("Enter the instruction:")
-
-    # Define original position
-    x, y = input("Enter original position:").split()
-    x = int(x)
-    y = int(y)
-    original_position = [x, y]
-    print("Original position:", original_position)
 
     # Ready to move
     print("Planned moving steps:", moves_lst)
@@ -25,17 +34,18 @@ def moves() -> object:
     # Calculate the new position
     for i in range(0, len(moves_lst)):
         if moves_lst[i] == 'U':
-            new_position = new_position + U
+            new_position = new_position + direction_u
 
         elif moves_lst[i] == 'D':
-            new_position = new_position + D
+            new_position = new_position + direction_d
 
         elif moves_lst[i] == 'L':
-            new_position = new_position + L
+            new_position = new_position + direction_l
 
         elif moves_lst[i] == 'R':
-            new_position = new_position + R
-        print("Step", i + 1, "New position:", new_position)
+            new_position = new_position + direction_r
+        if i < (len(moves_lst) - 1):
+            print("Step", i + 1, "New position:", new_position)
 
     # Compare final position to the original position
     print("Final position", new_position)
