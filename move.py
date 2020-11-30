d7e591937bcd170fb662423ff7c9066e0d733b81
Nolan <nolan.lu@veztan.com>
2020-11-30 16:57:46 +0800

    simply variable

--- move.py
+++ move.py
@@ -1,82 +1,71 @@
 import numpy as np;
 import sys
 
-def moves():
-    # Define direction
-    dict_direction = {"U": np.array([0, 1]),
-                      "D": np.array([0, -1]),
-                      "L": np.array([-1, 0]),
-                      "R": np.array([1, 0])
-                      }
 
-    allowed_characters = ['U', 'D', 'L', 'R']
-    max_retry = 5
+class Robot:
+    def moves():
+        # Define direction
+        dict_direction = {"U": np.array([0, 1]),
+                          "D": np.array([0, -1]),
+                          "L": np.array([-1, 0]),
+                          "R": np.array([1, 0])
+                          }
+        max_retry = 5
 
-    # Function: show error message
-    def err(msg):
-        print("Error times:", i + 1)
-        print(msg)
-        print("Retry times left:", max_retry - i - 1)
-        print("\n")
+        # Function: show error message
+        def err(msg):
+            print("Error times:", i + 1)
+            print(msg)
+            print("Retry times left:", max_retry - i - 1)
+            print("\n")
 
-    # Function: Compare value
-    def compare():
-        comparison = new_position == original_position
-        equal_arrays = comparison.all()
-        if equal_arrays:
-            print("Back to original position")
-        else:
-            print("Move away from the original position")
+        # Function: Compare value
+        def compare():
+            comparison = new_position == original_position
+            equal_arrays = comparison.all()
+            if equal_arrays:
+                print("Back to original position")
+            else:
+                print("Move away from the original position")
 
-    # Define original position
-    for i in range(0, max_retry):
-        try:
-            original_x, original_y = input("Enter numeric original position: ").replace(',', ' ').split()
-            original_x = int(original_x)
-            original_y = int(original_y)
-            original_position = [original_x, original_y]
-            print("Original position:", original_position)
-            break
-        except ValueError:
+        # Define original position
+        for i in range(0, max_retry):
+            try:
+                original_x, original_y = input("Enter numeric original position: ").replace(',', ' ').split()
+                new_position = original_position = [int(original_x), int(original_y)]
+                print("Original position:", original_position)
+                break
+            except ValueError:
+                if i == max_retry - 1:
+                    err("This program will be stopped here.")
+                    sys.exit(1)
+                else:
+                    err("Please enter TWO numbers")
+
+        # Define moving sequence
+        for i in range(0, max_retry):
+            moves_lst = input("Please enter the instruction. e.g. UDLR ").upper()
             if i == max_retry - 1:
                 err("This program will be stopped here.")
                 sys.exit(1)
+            elif any(x not in dict_direction.keys() for x in moves_lst):
+                err("Directional characters only")
             else:
-                err("Please enter TWO numbers")
+                print("Planned moving steps:", moves_lst)
+                break
 
-    # Define moving sequence
-    for i in range(0, max_retry):
-        moves_lst = input("Please enter the instruction. e.g. UDLR ").upper()
-        if i == max_retry - 1:
-            err("This program will be stopped here.")
-            sys.exit(1)
-        elif any(x not in allowed_characters for x in moves_lst):
-            err("Directional characters only")
-        else:
-            print("Planned moving steps:", moves_lst)
-            break
+        # Calculate new position
+        for i in range(0, len(moves_lst)):
+            if moves_lst[i] in dict_direction.keys():
+                new_position = new_position + dict_direction[moves_lst[i]]
+            if i < (len(moves_lst) - 1):
+                print("Step", i + 1, "New position:", new_position)
+            if i == len(moves_lst) - 1:
+                print("Final position: ", new_position)
 
-    new_position = original_position
-    for i in range(0, len(moves_lst)):
-        if moves_lst[i] == 'U':
-            new_position = new_position + dict_direction["U"]
-
-        elif moves_lst[i] == 'D':
-            new_position = new_position + dict_direction["D"]
-
-        elif moves_lst[i] == 'L':
-            new_position = new_position + dict_direction["L"]
-
-        elif moves_lst[i] == 'R':
-            new_position = new_position + dict_direction["R"]
-        if i < (len(moves_lst) - 1):
-            print("Step", i + 1, "New position:", new_position)
-        if i == len(moves_lst) - 1:
-            print("Final position: ", new_position)
-
-    # Compare final position to the original position
-    compare()
+        # Compare final position to the original position
+        compare()
 
 
 if __name__ == '__main__':
-    moves()
+    Robot.moves()
