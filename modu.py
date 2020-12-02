50b5d228caa8740ba33608408adb515162c5129a
Nolan <nolan.lu@veztan.com>
2020-12-02 08:25:57 +0800

    add more def()

--- modu.py
+++ modu.py
@@ -1,8 +1,9 @@
-import numpy as np;
+import numpy as np
+import math
 
 
-def dict():
-    # Define direction
+# Define direction
+def dict_info():
     dict_direction = {"U": np.array([0, 1]),
                       "D": np.array([0, -1]),
                       "L": np.array([-1, 0]),
@@ -11,6 +12,33 @@ def dict():
     return dict_direction
 
 
+# Define max retry times
 def max_retry():
     times = 5
     return times
+
+
+# Function: Compare value
+def compare(new_position, original_position):
+    comparison = new_position == original_position
+    equal_arrays = comparison.all()
+    distance(new_position, original_position)
+    if equal_arrays:
+        return print("Back to original position")
+    else:
+        return print("Move away from the original position")
+
+
+# Function: Calculate distance between twp points
+def distance(new_position, original_position):
+    length = math.dist(new_position, original_position)
+    print("Distance: ", length)
+
+
+# Function: error message
+def err(msg, i, max_retry):
+    print("Error times:", i + 1)
+    print(msg)
+    print("Retry times left:", max_retry - i - 1)
+    print("\n")
+
--- move.py
+++ move.py
@@ -1,72 +1,62 @@
-import numpy as np;
 import sys
-import math
 import modu
+
+
 class Robot:
     def moves():
         # Define direction
-        dict_direction = modu.dict()
+        dict_direction = modu.dict_info()
         max_retry = modu.max_retry()
-
-        # Function: show error message
-        def err(msg):
-            print("Error times:", i + 1)
-            print(msg)
-            print("Retry times left:", max_retry - i - 1)
-            print("\n")
-
-        # Function: Compare value
-        def compare():
-            comparison = new_position == original_position
-            equal_arrays = comparison.all()
-            if equal_arrays:
-                print("Back to original position")
-            else:
-                print("Move away from the original position")
-                distance()
-
-        # Function: Calculate distance between twp points
-        def distance():
-            length = (math.dist(original_position, new_position))
-            print("Distance: ", length)
+        position_list = []
 
         # Define original position
-        for i in range(0, max_retry):
-            try:
-                original_x, original_y = input("Enter numeric original position: ").replace(',', ' ').split()
-                new_position = original_position = [int(original_x), int(original_y)]
-                print("Original position:", original_position)
-                break
-            except ValueError:
-                if i == max_retry - 1:
-                    err("This program will be stopped here.")
-                    sys.exit(1)
-                else:
-                    err("Please enter TWO numbers")
+        def new_start():
+            for i in range(0, max_retry):
+                try:
+                    original_x, original_y = input("Enter numeric original position: ").replace(',', ' ').split()
+                    new_position = original_position = [int(original_x), int(original_y)]
+                    position_list.append(new_position)
+                    print("Original position:", original_position)
+                    break
+                except ValueError:
+                    if i == max_retry - 1:
+                        modu.err("This program will be stopped here.", i, max_retry)
+                        sys.exit(1)
+                    else:
+                        modu.err("Please enter TWO numbers", i, max_retry)
+
+            return new_position, original_position, position_list
+
+        new_position, original_position, position_list = new_start()
 
         # Define moving sequence
-        for i in range(0, max_retry):
-            moves_lst = input("Please enter the instruction. e.g. UDLR ").upper()
-            if i == max_retry - 1:
-                err("This program will be stopped here.")
-                sys.exit(1)
-            elif any(x not in dict_direction.keys() for x in moves_lst):
-                err("Directional characters only")
-            else:
-                print("Planned moving steps:", moves_lst)
-                break
+        def new_list():
+            for i in range(0, max_retry):
+                moves_lst = input("Please enter the instruction. e.g. UDLR ").upper()
+                if i == max_retry - 1:
+                    modu.err("This program will be stopped here.", i, max_retry)
+                    sys.exit(1)
+                elif any(x not in dict_direction.keys() for x in moves_lst):
+                    modu.err("Directional characters only", i, max_retry)
+                else:
+                    print("Planned moving steps:", moves_lst)
+                    break
+            return moves_lst
+
+        moves_lst = new_list()
 
         # Calculate new position
         for i in range(0, len(moves_lst)):
-            if moves_lst[i] in dict_direction.keys():
-                new_position = new_position + dict_direction[moves_lst[i]]
-            if i < (len(moves_lst) - 1):
-                print("Step", i + 1, "New position:", new_position)
-            if i == len(moves_lst) - 1:
-                print("Final position: ", new_position)
+                if moves_lst[i] in dict_direction.keys():
+                    new_position = new_position + dict_direction[moves_lst[i]]
+                    position_list.append(new_position)
+                if i < (len(moves_lst) - 1):
+                    print("Step", i + 1, "New position:", new_position)
+                else:
+                    print("Final position: ", new_position)
 
         # Compare final position to the original position
-        compare()
+        modu.compare(new_position, original_position)
 
 
 if __name__ == '__main__':
